61d2ccef85a7ed501dd5074ea4af2c7fb43aad58
Yugenderan Meganathan <yugenderanm@outlook.com>
2023-02-18 13:42:28 +0530

    Updated Client object class definition with session inbuilt

--- Scripts/tradingclasses.py
+++ Scripts/tradingclasses.py
@@ -10,6 +10,8 @@ from dhanhq import dhanhq
 from generalclasses import General
 from datetime import datetime as dt, date, time
 import pandas as pd
+import requests
+import json
 
 
 class Client:
@@ -25,9 +27,21 @@ class Client:
         self.authenticator_id = client_data['authenticator_id']
         self.access_token = client_data['access_token']
         self.capital = client_data[strategy]
-    
+        #Create New trading Session
+        if self.broker == 'zerodha':
+            session = KiteConnect(api_key=self.api_key)
+            session.set_access_token(self.access_token)
+        
+        elif self.broker == 'dhan':
+            session = dhanhq(self.broker_id, self.access_token)
+        
+        self.session = session
+        
+###############################################################################       
+class Trading:
+    '''contains all methods needed for trading'''
     def session(self, user, clients):
-        '''Create New Kite Session'''
+        '''Create New trading Session'''
         if clients[user].broker == 'zerodha':
             session = KiteConnect(api_key=clients[user].api_key)
             session.set_access_token(clients[user].access_token)
@@ -36,10 +50,7 @@ class Client:
             session = dhanhq(clients[user].broker_id, clients[user].access_token)
         
         return session
-
-###############################################################################       
-class Trading:
-    '''contains all methods needed for trading'''
+    
     def instr_list(self, session, underlying, exchange, strike_diff, no_of_strikes):
         '''use only zerodha session for this method'''
         lp = session.ltp(exchange + ':' + underlying)
@@ -75,7 +86,96 @@ class Trading:
         if ratio > 1:
             ratio = 1
         return ratio
+    
+    def get_iv(self, instrument_list, scrip):
+        ## Importing Options Chain From NSE
+        # Urls for fetching Data
+        url_oc      = "https://www.nseindia.com/option-chain"
+        url_bnf     = 'https://www.nseindia.com/api/option-chain-indices?symbol=BANKNIFTY'
+        url_nf      = 'https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY'
+        url_indices = "https://www.nseindia.com/api/allIndices"
+        if scrip =='NIFTY':
+            url = url_nf
+        elif scrip == 'BANKNIFTY':
+            url = url_bnf
+        # Headers
+        headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36',
+                    'accept-language': 'en,gu;q=0.9,hi;q=0.8',
+                    'accept-encoding': 'gzip, deflate, br'}
+        sess = requests.Session()
+        cookies = dict()
+        # Local methods
+        def set_cookie():
+            request = sess.get(url_oc, headers=headers, timeout=5)
+            cookies = dict(request.cookies)
+        def get_data(url):
+            set_cookie()
+            response = sess.get(url, headers=headers, timeout=5, cookies=cookies)
+            if(response.status_code==401):
+                set_cookie()
+                response = sess.get(url_nf, headers=headers, timeout=5, cookies=cookies)
+            if(response.status_code==200):
+                return response.text
+            return ""
+        response_text = get_data(url)
+        data = json.loads(response_text)
+        # Fetching CE and PE data based on Nearest Expiry Date
+        #currExpiryDate = data["records"]["expiryDates"][0]
+        d2 = data["filtered"]["data"]
+        CE = []
+        PE = []
+        for item in d2:
+            for k,v in item.items():
+                if k == 'CE':
+                    CE.append(v)
+                elif k == 'PE':
+                    PE.append(v)
+        ce_df = pd.DataFrame(CE)
+        ce_df['instrument_type'] = 'CE'
+        pe_df = pd.DataFrame(PE)
+        pe_df['instrument_type'] = 'PE'
+        left = instrument_list[['strike', 'instrument_token', 'instrument_type']]
+        right1 = ce_df[['strikePrice', 'impliedVolatility','instrument_type']]
+        right2 = pe_df[['strikePrice', 'impliedVolatility','instrument_type']]
+        ce_iv = pd.merge(left, right1, left_on=['strike', 'instrument_type'], 
+                      right_on=['strikePrice', 'instrument_type'])
+        pe_iv = pd.merge(left, right2, left_on=['strike', 'instrument_type'], 
+                      right_on=['strikePrice', 'instrument_type'])
+        #ce__df = ce_df[['strikePrice', 'impliedVolatility']]
+        iv = pd.concat([ce_iv, pe_iv],axis = 0, join = 'outer', ignore_index=True)
+        iv = iv.sort_values('strike').reset_index(drop=True).drop(columns=['strikePrice'])
+        iv = iv.rename(columns={'impliedVolatility':'iv'})
+        iv.to_csv('files\\impliedVolatility.csv', index =False)
+        return iv
         
         
-        
+        def strangle_selection(kite_session, instrument_list, strike_diff, exchange, scrip):
+            '''Strike selection based on given strike difference from atm'''
+            #getting last traded price from kiteconnect
+            exchange = 'NSE'
+            scrip = 'NIFTY BANK'
+            ltp = kite_session.ltp(exchange +':'+scrip)
+            #converting data received to pandas dataframe
+            data = {}
+            data = pd.DataFrame(data, index=["spot_last_price"])
+            for num in range(0,4):
+                data[num]= ltp["NSE:NIFTY BANK"]["last_price"]
+           
+            data = data.T
+            data["name"] = "BANKNIFTY"
+            data["instrument_type"] = ["CE", "CE", "PE", "PE"]
+            data["strike_difference"] = [1500,1000,-1000,-1500]
+            #data = data[["name","spot_last_price"]]
+           
+            #finding atm and strike
+            data["atm"] = round_multiple(data["spot_last_price"],100)
+            data["strike"] = data["atm"] + data["strike_difference"]
+           
+           
+            #getting symbol for selected strike prices
+            symbols = pd.merge(data,instrument_list,left_on = ["name","instrument_type","strike"],
+                                right_on=["name","instrument_type","strike"], how ="inner",)
+            symbols = symbols.drop("last_price",axis = 1)
+            return symbols
+            
         
